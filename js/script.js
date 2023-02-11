d4bae98afb1acafeb26af1835e5fada37a80854a
jaw_breaker <muhammadh1445@gmail.com>
2023-02-11 07:08:20 +0530

    arrow clickable update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function init () {
     document.querySelector('.faq-question').addEventListener('click', function(event) {
+        console.log(event.target.tagName)
         if (event.target.tagName === 'H2') {
             header = event.target
             arrow =  event.target.nextElementSibling
@@ -8,6 +9,13 @@ function init () {
             arrow.classList.toggle('active')
             answer.classList.toggle('hidden')
             
+        } else if (event.target.tagName === 'IMG') {
+            arrow = event.target
+            header =  event.target.previousElementSibling
+            answer = event.target.parentElement.nextElementSibling
+            header.classList.toggle('header-active')
+            arrow.classList.toggle('active')
+            answer.classList.toggle('hidden')
         } else {
             // do nothing
         }
